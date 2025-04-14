e7bc000b20a1eac776fdf5b22fc2dad97579c388
minhlam284 <ndminhlam.284@gmail.com>
2025-04-14 16:52:18 +0700

    config

---
+++ config/local_config.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+at_biwi = False  # Are you running this code from the ETH Computer Vision Lab (Biwi)?
+
+project_root = '/Users/kaiser_1/Documents/GitHub/PHi_Seg'
+log_root = '/Users/kaiser_1/Documents/GitHub/PHi_Seg/log'
