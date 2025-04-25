b507be98504e29575d7aa0585f8197f814bdb717
minhlam284 <ndminhlam.284@gmail.com>
2025-04-25 14:01:09 +0700

    fix experiment

--- log/mmis/PHISegBig/experiment.py
+++ log/mmis/PHISegBig/experiment.py
@@ -2,15 +2,19 @@ import torch
 import torch.nn as nn
 from model.phiseg import PHISeg
 from utils import normalise_image
+experiment_name = 'PHISegBig'
+log_dir_name = 'mmis'
 
-experiment_name = 'PHISeg_7_5'
-log_dir_name = 'msmri'
+data_dir = '/mnt/apple/k66/minhlam/phiseg/mmis'
+mask_type = 'random'
+num_workers = 4
+
 
 # number of filter for the latent levels, they will be applied in the order as loaded into the list
-filter_channels = [32, 64, 128, 192, 192, 192, 192]
+filter_channels = [32, 64, 128, 192, 256, 256, 256]
 latent_levels = 5
 
-iterations = 500000
+# iterations = 2
 
 n_classes = 2
 num_labels_per_subject = 4
@@ -23,8 +27,8 @@ exponential_weighting = True
 
 # use 1 for grayscale, 3 for RGB images
 input_channels = 3
-epochs_to_train = 20
-batch_size = 128
+epochs_to_train = 500
+batch_size = 32
 image_size = (3, 128, 128)
 
 augmentation_options = {'do_flip_lr': True,
@@ -38,11 +42,11 @@ input_normalisation = normalise_image
 validation_samples = 16
 num_validation_images = 100
 
-logging_frequency = 1000
-validation_frequency = 1
+logging_frequency = 50
+validation_frequency = 50
 
 weight_decay = 10e-5
 
-pretrained_model = None #'PHISeg_best_ged.pth'
+pretrained_model = None
 # model
 model = PHISeg
