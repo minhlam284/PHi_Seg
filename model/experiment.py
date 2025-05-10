e11879c62bd5f482a7c55a79fbb3e248e2e92b50
minhlam284 <ndminhlam.284@gmail.com>
2025-05-10 15:09:49 +0700

    fix model

--- model/experiment.py
+++ model/experiment.py
@@ -4,13 +4,13 @@ from model.phiseg import PHISeg
 from utils import normalise_image
 
 experiment_name = 'PHISeg_7_5'
-log_dir_name = 'msmri'
+log_dir_name = 'mmis_test'
 
 # number of filter for the latent levels, they will be applied in the order as loaded into the list
 filter_channels = [32, 64, 128, 192, 192, 192, 192]
 latent_levels = 5
 
-iterations = 500000
+iterations = 500
 
 n_classes = 2
 num_labels_per_subject = 4
@@ -24,7 +24,7 @@ exponential_weighting = True
 # use 1 for grayscale, 3 for RGB images
 input_channels = 3
 epochs_to_train = 20
-batch_size = 128
+batch_size = 32
 image_size = (3, 128, 128)
 
 augmentation_options = {'do_flip_lr': True,
@@ -38,8 +38,8 @@ input_normalisation = normalise_image
 validation_samples = 16
 num_validation_images = 100
 
-logging_frequency = 1000
-validation_frequency = 1
+logging_frequency = 25
+validation_frequency = 25
 
 weight_decay = 10e-5
 
